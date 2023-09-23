49b24d5e72a6be516dc2284ab2b027b706d058ab
Gabriel <wenck13@gmail.com>
2023-09-23 20:21:05 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/giwc/study/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.giwc.study.repositories;
+
+import com.giwc.study.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
