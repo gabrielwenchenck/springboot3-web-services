fbeaea10c2517b120d2fa002e09c7d4c2f211904
Gabriel <wenck13@gmail.com>
2023-09-23 20:47:36 -0300

    Service layer, component registration

---
+++ src/main/java/com/giwc/study/services/UserService.java
@@ -0,0 +1,27 @@
+package com.giwc.study.services;
+
+import com.giwc.study.entities.User;
+import com.giwc.study.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
