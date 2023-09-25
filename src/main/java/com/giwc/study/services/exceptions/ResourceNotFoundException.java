6a3bc5cfe8f43ce0c56293fc68d0ea1e4c9dbf35
Gabriel <wenck13@gmail.com>
2023-09-25 19:04:16 -0300

    Exception handling - findById

---
+++ src/main/java/com/giwc/study/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.giwc.study.services.exceptions;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+
+
+}
